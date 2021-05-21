f85f68f0ac766f5871398fb9cb5fef98800e27ab
Stephanie George <stephaniegeorge@Stephanies-MacBook-Air.local>
2021-05-21 01:27:23 -0400

    show todays date

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,11 +1,16 @@
+var nowDate = moment().format('dddd, MMM Do YYYY');
+$("#currentDay").html(nowDate);
+
+
 // each time block is color coded to indicate if time if past(gray), present(red), and future(green)
 
 // 1. assign time to container 
 // 2. use moment to get current time 
-var time = moment().format('LTS'); 
-console.log(time);
+var nowTime = moment().format('HH');
+console.log(nowTime);
 
 // 3. use four loop to loop through time and assign colors based on hour 
-    // if hour < present hour then gray 
-    // if hour = present hour then red
-    // if hour > present then green 
+
+//     // if hour < present hour then gray 
+//     // if hour = present hour then red
+//     // if hour > present then green 
