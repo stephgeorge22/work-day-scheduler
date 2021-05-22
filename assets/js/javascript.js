4c575b0f36ff535d6eb2955a252dc7f29b23cc30
Stephanie George <stephaniegeorge@Stephanies-Air.T-mobile.com>
2021-05-21 23:36:51 -0400

    color code tasks based on color

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,16 +1,31 @@
 var nowDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(nowDate);
 
-
 // each time block is color coded to indicate if time if past(gray), present(red), and future(green)
+function checkTime() {
+    // 1. assign time to container 
+    
+    // 2. use moment to get current time 
+    var nowTime = moment().format('HH');
 
-// 1. assign time to container 
-// 2. use moment to get current time 
-var nowTime = moment().format('HH');
-console.log(nowTime);
+    // 3. compare time
+    $('.task').each(function() {
+        var colorCode = this.attributes[2].nodeValue;
 
-// 3. use four loop to loop through time and assign colors based on hour 
+        // if hour < present hour then gray 
+        if (colorCode < nowTime) {
+            $(".task[value = "+colorCode+"]").css({'background-color':'gray'});
+        }
 
-//     // if hour < present hour then gray 
-//     // if hour = present hour then red
-//     // if hour > present then green 
+        // if hour = present hour then red
+        else if (colorCode === nowTime) {
+            $(".task[value = "+colorCode+"]").css({'background-color':'red'});
+        }
+        // if hour > present then green 
+        else {
+            $(".task[value = "+colorCode+"]").css({'background-color':'green'});
+        }
+    });
+};
+
+checkTime();
